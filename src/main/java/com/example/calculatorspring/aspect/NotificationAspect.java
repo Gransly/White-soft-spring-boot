ad3963c52244efd6bf33ca15f1fdadc13a6de0a4
Gransly <bob031299@gmail.com>
2021-08-14 14:27:01 +1000

    Aspect working after all rest requests

--- src/main/java/com/example/calculatorspring/aspect/NotificationAspect.java
+++ src/main/java/com/example/calculatorspring/aspect/NotificationAspect.java
@@ -1,16 +1,18 @@
 package com.example.calculatorspring.aspect;
 
+import com.example.calculatorspring.entity.NotificationMessage;
 import com.example.calculatorspring.service.notification.Notification;
-import com.example.calculatorspring.service.notification.NotificationMessage;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @Aspect
 @Component
 @RequiredArgsConstructor
@@ -18,19 +20,35 @@ public class NotificationAspect {
 
     private final List<Notification> notifiers;
 
-    @After("execution(* com.example.calculatorspring.controller.ApiExceptionHandler.*(..))")
-    public void notifyAboutRequest(JoinPoint jp){
+    @Around(value = "@annotation(notify)")
+    public Object notifyAboutRequest(ProceedingJoinPoint joinPoint, Notify notify) throws Throwable {
 
         LocalDateTime callTime = LocalDateTime.now();
-        String methodName = jp.getSignature().getName();
 
+        String methodName = joinPoint.getSignature().getName();
+
+        String exceptionClassName = "";
+
+        Object proceed;
+
+        try {
+            proceed = joinPoint.proceed();
+        } catch (Exception e) {
+
+            exceptionClassName =e.getMessage();
+            throw e;
+        } finally {
             NotificationMessage message = NotificationMessage.builder()
                                                              .callTime(callTime)
                                                              .methodName(methodName)
+                                                             .description(notify.description())
+                                                             .exceptionName(exceptionClassName)
                                                              .build();
 
             for (Notification notification : notifiers) {
                 notification.sendNotification(message);
             }
+        }
+        return proceed;
     }
 }
--- src/test/java/com/example/calculatorspring/aspect/NotificationAspectTest.java
+++ src/test/java/com/example/calculatorspring/aspect/NotificationAspectTest.java
@@ -1,9 +1,9 @@
 package com.example.calculatorspring.aspect;
 
-import com.example.calculatorspring.service.notification.NotificationMessage;
+import com.example.calculatorspring.check.exception.InputForbiddenException;
+import com.example.calculatorspring.entity.NotificationMessage;
 import com.example.calculatorspring.service.notification.TelegramBotNotification;
 import com.google.common.collect.Lists;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
@@ -15,39 +15,66 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@ExtendWith(SoftAssertionsExtension.class)
 class NotificationAspectTest {
 
     @Mock
     private TelegramBotNotification telegramBotNotification;
 
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    private JoinPoint joinPoint;
+    private ProceedingJoinPoint joinPoint;
 
+    @Mock
+    private Notify notify;
 
     @Captor
     ArgumentCaptor<NotificationMessage> messageCaptor;
 
     @Test
-    void notifyAboutRequest() {
+    void notifyAboutRequest(SoftAssertions softly) throws Throwable {
 
         //Arrange
         NotificationAspect aspect = new NotificationAspect(Lists.newArrayList(telegramBotNotification));
 
-        when(joinPoint.getSignature().getName()).thenReturn("getStatsByParam");
+        when(joinPoint.getSignature().getName()).thenReturn("Name");
+        when(notify.description()).thenReturn("Description");
 
         //Act
-        aspect.notifyAboutRequest(joinPoint);
+        aspect.notifyAboutRequest(joinPoint, notify);
 
         //Assert
         verify(telegramBotNotification, times(1)).sendNotification(messageCaptor.capture());
 
         NotificationMessage actualMessage = messageCaptor.getValue();
 
-        assertEquals(actualMessage.getMethodName(), "getStatsByParam");
+        softly.assertThat(actualMessage.getDescription()).isEqualTo("Description");
+        softly.assertThat(actualMessage.getMethodName()).isEqualTo("Name");
 
+    }
+
+    @Test
+    void notifyAboutExceptionThrow(SoftAssertions softly) throws Throwable {
+        //Arrange
+        NotificationAspect aspect = new NotificationAspect(Lists.newArrayList(telegramBotNotification));
+
+        when(joinPoint.getSignature().getName()).thenReturn("Name");
+        when(joinPoint.proceed()).thenThrow(new InputForbiddenException("Error, input should contain number>0"));
+
+        when(notify.description()).thenReturn("Description");
+
+        //Act & assert
+        assertThrows(InputForbiddenException .class, () -> aspect.notifyAboutRequest(joinPoint, notify));
+        verify(telegramBotNotification, times(1))
+                .sendNotification(messageCaptor.capture());
+
+        NotificationMessage actualMessage = messageCaptor.getValue();
+
+        softly.assertThat(actualMessage.getDescription()).isEqualTo("Description");
+        softly.assertThat(actualMessage.getMethodName()).isEqualTo("Name");
+        softly.assertThat(actualMessage.getExceptionName()).isEqualTo("Error, input should contain number>0");
     }
 }
